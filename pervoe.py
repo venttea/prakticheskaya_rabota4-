e8e13a52e03f1cc6861eb79814d84d4d061745c4
venttea <keshasolnshko@gmail.com>
2025-04-11 09:31:43 +0700

    Update pervoe.py

--- pervoe.py
+++ pervoe.py
@@ -14,3 +14,15 @@ class Music(Media):
 class Video(Media):
     def play(self):
         return "Playing video file"
+
+media = Media()
+print(media.play())
+print(media.pause())
+
+music = Music()
+print(music.play())
+print(music.pause())
+
+video = Video()
+print(video.play())
+print(video.pause())
